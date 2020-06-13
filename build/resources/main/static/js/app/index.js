5c8c0b9d6f4ea09416baed2b0004aad1d7212107
myeongheeroh <myeongheeroh@gmail.com>
2020-06-13 18:27:06 +0900

    Posts Update

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -6,6 +6,9 @@ var main = {
         $('#btn-save').on('click', function() {
             _this.save();
             });
+        $('#btn-update').on('click', function () {
+            _this.update();
+            });
     },
     save : function (){
         var data = {
@@ -18,14 +21,38 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             datatype: 'json',
-            contentType: 'application/json; charset=utf-8', data: JSON.stringify(data)
-        }).done(function(){
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function(a){
+            alert(a);
                 alert('글이 등록되었습니다.');
                 window.location.href = '/';
         }).fail(function (error){
             alert(JSON.stringify(error));
         });
+    },
+
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
-}
+};
 
 main.init();
